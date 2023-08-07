1f084b84a10a02c0a6356d5bf95bd9cdee3bc118
Ashen Ishanka <ashen.ishanka74@gmail.com>
2023-08-07 09:51:05 +0530

    Initial Commit

---
+++ SampleAppOpenAds/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleAppOpenAds
+//
+//  Created by Ashen Ishanka on 2023-08-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
